b1ffad8fe2b5742e05c3b6c738940dd993ce33f2
Graham Strickland <grahamhstrickland@gmail.com>
2025-02-25 18:33:15 +0200

    Add julday algorithm and documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+/// This routine calculates the phase of the moon. Given an integer `n` and
+/// a code `nph` for the phase desired (`nph = 0` for new moon, `1` for first quarter, `2` for
+/// full, `3` for last quarter), the routine returns the Julian Day Number `julian_date`, and
+/// the fractional part of a day `frac` to be added to it, of the `n`th such phase since January,
+/// 1900. Greenwich Mean Time is assumed.
 pub fn flmoon(n: i32, nph: i32) -> (i64, f64) {
     let c = f64::from(n + nph) / 4.0;
     let t = c / 1236.85;
@@ -29,9 +34,48 @@ pub fn flmoon(n: i32, nph: i32) -> (i64, f64) {
     (julian_date, frac)
 }
 
+const IGREG: i64 = 15 + 31 * (10 + 12 * 1582);
+
+/// In this routine `julday` returns the Julian Day Number that begins at noon of the calendar date
+/// specified by month `mm`, day `id`, and year `iyyy`, all integer variables. Positive year
+/// signifies A.D.; negative, B.C. Remember that the year after 1 B.C. was 1 A.D.
+pub fn julday(mm: i32, id: i32, iyyy: i32) -> i64 {
+    let mut jul;
+    let ja;
+    let jm;
+    let mut jy = iyyy;
+
+    if jy == 0 {
+        panic!("julday: there is no year zero.");
+    }
+
+    if jy < 0 {
+        jy += 1;
+    }
+
+    if mm > 2 {
+        jm = mm + 1;
+    } else {
+        jy -= 1;
+        jm = mm + 13;
+    }
+
+    jul = f64::floor(365.25 * f64::from(jy))
+        + f64::floor(30.6001 * f64::from(jm))
+        + f64::from(id)
+        + 1720995.0;
+
+    if i64::from(id + 31 * (mm + 12 * iyyy)) >= IGREG {
+        ja = (0.01 * f64::from(jy)) as i32;
+        jul += 2.0 - f64::from(ja) + f64::floor(0.25 * f64::from(ja));
+    }
+
+    jul as i64
+}
+
 #[cfg(test)]
 mod tests {
-    use super::flmoon;
+    use super::{flmoon, julday};
 
     #[test]
     fn test_flmoon() {
@@ -50,5 +94,22 @@ mod tests {
     #[should_panic]
     fn test_flmoon_panic() {
         let (_, _) = flmoon(21, 5);
+    }
+
+    #[test]
+    fn test_julday() {
+        let mut res = julday(1, 28, 1991);
+
+        assert_eq!(res, 2448285);
+
+        res = julday(2, 25, 2025);
+
+        assert_eq!(res, 2460732);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_julday_panic() {
+        let _ = julday(0, 0, 0);
     }
 }
